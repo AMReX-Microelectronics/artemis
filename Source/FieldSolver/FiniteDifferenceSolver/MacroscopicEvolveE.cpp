843a5672dff63afb25f5a58e072c6e8bbd4189f3
Revathi  Jambunathan <41089244+RevathiJambunathan@users.noreply.github.com>
2021-08-11 21:36:14 -0700

    turning on LLG as default in cmake (#26)
    * turning on LLG as default in cmake
    
    * remove files that are not in repo anymore
    
    * add target as variable
    
    * unused parameter warning leads to error in CIO
    
    * remove redundant semicolon and initialize Mfield
    
    * Update Source/FieldSolver/FiniteDifferenceSolver/MacroscopicProperties/MacroscopicProperties.H
    
    * ignore unused
    
    * ignore unused
    
    * ignore unused
    
    * fix unused Bfield and Hfield
    
    * Update Source/FieldSolver/FiniteDifferenceSolver/MacroscopicEvolveEPML.cpp
    
    * ensure the functions return something
    
    * compile HM files ifndef WARPX_DIM_RZ
    
    * fix headers
    
    * ignore unused for RZ
    
    * use nodal flag instead of hard-coded intvect
    
    * compile with RZ
    
    * more RZ build fixes
    
    * do not run laser input for LLG build in intel workflow CI
    
    * instead of hardcoding IntVect use the staggering of multifab
    
    * turn off running laser input
    
    * dont run pywarpx for laser test case in ubuntu CI
    
    * fix typo
    
    * remove print statement
    
    * remove printf statements so SYCL does not complain

--- Source/FieldSolver/FiniteDifferenceSolver/MacroscopicEvolveE.cpp
+++ Source/FieldSolver/FiniteDifferenceSolver/MacroscopicEvolveE.cpp
@@ -45,7 +45,11 @@ void FiniteDifferenceSolver::MacroscopicEvolveE (
    // Select algorithm (The choice of algorithm is a runtime option,
    // but we compile code for each algorithm, using templates)
 #ifdef WARPX_DIM_RZ
+#    ifndef WARPX_MAG_LLG
     amrex::ignore_unused(Efield, Bfield, Jfield, dt, macroscopic_properties);
+#    else
+    amrex::ignore_unused(Efield, Hfield, Jfield, dt, macroscopic_properties);
+#endif
     amrex::Abort("currently macro E-push does not work for RZ");
 #else
     if (m_do_nodal) {
@@ -127,7 +131,9 @@ void FiniteDifferenceSolver::MacroscopicEvolveECartesian (
 
     auto& sigma_mf = macroscopic_properties->getsigma_mf();
     auto& epsilon_mf = macroscopic_properties->getepsilon_mf();
+#ifndef WARPX_MAG_LLG
     auto& mu_mf = macroscopic_properties->getmu_mf();
+#endif
 
     // Index type required for calling CoarsenIO::Interp to interpolate macroscopic
     // properties from their respective staggering to the Ex, Ey, Ez locations
@@ -162,7 +168,9 @@ void FiniteDifferenceSolver::MacroscopicEvolveECartesian (
         // material prop //
         Array4<Real> const& sigma_arr = sigma_mf.array(mfi);
         Array4<Real> const& eps_arr = epsilon_mf.array(mfi);
+#ifndef WARPX_MAG_LLG
         Array4<Real> const& mu_arr = mu_mf.array(mfi);
+#endif
 
         // Extract stencil coefficients
         Real const * const AMREX_RESTRICT coefs_x = m_stencil_coefs_x.dataPtr();
