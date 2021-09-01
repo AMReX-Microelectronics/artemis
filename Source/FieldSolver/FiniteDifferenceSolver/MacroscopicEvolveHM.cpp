bacb05a12a3534ff0972c02e79d223c3d61715a8
Prabhat Kumar <89051199+prkkumar@users.noreply.github.com>
2021-09-01 09:50:36 -0700

    Update Source/FieldSolver/FiniteDifferenceSolver/MacroscopicEvolveHM.cpp

--- Source/FieldSolver/FiniteDifferenceSolver/MacroscopicEvolveHM.cpp
+++ Source/FieldSolver/FiniteDifferenceSolver/MacroscopicEvolveHM.cpp
@@ -72,7 +72,6 @@ void FiniteDifferenceSolver::MacroscopicEvolveHMCartesian(
     // temporary Multifab storing M from previous timestep (old_time) before updating to M(new_time)
     std::array<std::unique_ptr<amrex::MultiFab>, 3> Mfield_old; // Mfield_old is M(old_time)
 
-    //amrex::GpuArray<int, 3> const& mag_Ms_stag         = macroscopic_properties->mag_Ms_IndexType;
     amrex::GpuArray<int, 3> const& mag_alpha_stag      = macroscopic_properties->mag_alpha_IndexType;
     amrex::GpuArray<int, 3> const& mag_gamma_stag      = macroscopic_properties->mag_gamma_IndexType;
     amrex::GpuArray<int, 3> const& mag_exchange_stag   = macroscopic_properties->mag_exchange_IndexType;
